991f9fddaceacad06a9b22d8aec4e4d569bfde23
abel-baker <seth.keating@gmail.com>
2022-05-19 02:32:27 -0400

    models and associations

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,18 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    }
   },
   {
     sequelize,
